b88db1b4be2a570626d8b53ef12606417af054a5
AdenEnnis02 <aden.ennis1@gmail.com>
2023-12-12 00:40:32 -0700

    week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,21 +1,30 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value == Data)
+        {
+            return;
+        }
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -24,13 +33,28 @@ public class Node {
         }
     }
 
-    public bool Contains(int value) {
-        // TODO Start Problem 2
+    public bool Contains(int value)
+    {
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value);
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value);
+        }
         return false;
     }
 
-    public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+    public int GetHeight()
+    {
+        int leftHeight = (Left == null) ? 0 : Left.GetHeight();
+        int rightHeight = (Right == null) ? 0 : Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
